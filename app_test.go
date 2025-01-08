2f4954799670f03aed39abcc8243523a50b239d4
jang whoemoon <palindrom615@gmail.com>
2025-01-08 17:47:11 +0900

    test: addTemplateFunc case

--- app_test.go
+++ app_test.go
@@ -15,11 +15,19 @@ func TestRender(t *testing.T) {
 
 func TestTemplate_Include(t *testing.T) {
 	app := NewApp("test/out", "test/addTemplateFunc", "")
-	rendered, err := app.tmpl.Include("add.tmpl", "test/addTemplateFunc/add.star")
+	rendered, err := app.tmpl.Include("add.tmpl", "")
 	if err != nil {
 		t.Errorf("Error rendering template: %v", err)
 	}
 	if rendered != "1 + 1 = 2" {
 		t.Errorf("Expected `1 + 1 = 2`, got %s", rendered)
+	}
+
+	rendered, err = app.tmpl.Include("addstr.tmpl", "")
+	if err != nil {
+		t.Errorf("Error rendering template: %v", err)
+	}
+	if rendered != "a + b = ab" {
+		t.Errorf("Expected `a + b = ab`, got %s", rendered)
 	}
 }
